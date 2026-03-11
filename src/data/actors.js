7f79852392f37e908c1fcd24d49feb5c750210ef
Claire Hassig <processblueproductions@gmail.com>
2026-03-10 22:01:41 -0400

    Added Valjean

--- src/data/actors.js
+++ src/data/actors.js
@@ -1,3 +1,31 @@
-
+import { Actor } from './actor.js';
+import { ActorDetails } from './actorDetails.js';
+import { ActorReputation } from './actorReputation.js';
 
 // Jean Valjean
+export const jeanValjean = new Actor
+(
+  'Jean Valjean',
+  'Valjean',
+  'Les Misérables',
+  undefined,
+  new ActorDetails
+  (
+    {
+      strength: 18,
+      dexterity: 10,
+      constitution: 16,
+      intelligence: 12,
+      wisdom: 14,
+      charisma: 8
+    },
+    {
+      gameSavvy: 3,
+      honesty: 8,
+      verbosity: 3,
+      sensitivity: 5
+    },
+    [],
+    []
+  ),
+);
